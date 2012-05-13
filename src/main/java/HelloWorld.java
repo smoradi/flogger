56a3274dbf87b141d24960d92ea530ca5a18f628
Sid Marcopolo <sid.moradi@gmail.com>
2012-05-13 20:16:39 +0100

    Adding one more post(goingToMountain.html) and make posts accessiable by two ways: directly and directly (one of them is a little bit indirectly).

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,19 +3,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.*;
 
+@SuppressWarnings("serial")
 public class HelloWorld extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.getWriter().print("Hello from Java!\n");
+        String defaultPost= (req.getParameter("name") != null ? req.getParameter("name") : "startMyBlog.xml");
         try {
-        	FileReader _input= new FileReader("data/startMyBlog.xml");
-        	for (int i=0; i < 2085; i++)
-        		resp.getWriter().print((char) _input.read());
+        	FileReader _input= new FileReader("data/" + defaultPost);
+        	int c;
+        	while(-1 != (c = _input.read()))
+        		resp.getWriter().print((char) c);
         	try {
         		_input.close();
         	} catch (IOException e) {
@@ -29,9 +37,17 @@ public class HelloWorld extends HttpServlet {
     public static void main(String[] args) throws Exception{
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        context.setContextPath("/app");
+        context.addServlet(new ServletHolder(new HelloWorld()),"/file/*");
+        
+        ResourceHandler resourceHandler= new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase(".");
+        
+        HandlerList handlers= new HandlerList();
+        handlers.setHandlers(new Handler[] {context, resourceHandler, new DefaultHandler()});
+        server.setHandler(handlers);
+        
         server.start();
         server.join();   
     }
